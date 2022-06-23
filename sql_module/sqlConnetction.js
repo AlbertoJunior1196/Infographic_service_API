b43cec7ec3afee8eb6615a25760d796464f1a1b4
Alberto <a21270778@isec.pt>
2022-06-23 23:52:46 +0100

    connection host

--- sql_module/sqlConnetction.js
+++ sql_module/sqlConnetction.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
     host: "host.docker.internal",
-    port:'3306',
+    port:'3307',
     user: "root",
     password: "#password1196",
     database:"db_infographics"

a2a70008da464d4b0cf0dd73ce3b6a7e08fae6e7
Alberto <a21270778@isec.pt>
2022-06-23 23:43:05 +0100

    database host

--- sql_module/sqlConnetction.js
+++ sql_module/sqlConnetction.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-    host: "mysql",
-    port:'3306',
+    host: "localhost",
+    port:'3307',
     user: "root",
     password: "#password1196",
     database:"db_infographics"

b4ad06d7096a740fdb68984473793db7fdc565c0
Alberto <a21270778@isec.pt>
2022-06-24 21:45:03 +0100

    host name

--- sql_module/sqlConnetction.js
+++ sql_module/sqlConnetction.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql2')
 var connection = mysql.createConnection({
-    host: "192.168.99.100",
+    host: "172.17.0.1",
     port:'3306',
     user: "root",
     password: "#password1196",

7c30052b8793393f6bd9402622dc70552729f993
Alberto <a21270778@isec.pt>
2022-06-24 18:04:39 +0100

    correction host docker

--- sql_module/sqlConnetction.js
+++ sql_module/sqlConnetction.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql2')
 var connection = mysql.createConnection({
     host: "host.docker.internal",
-    port:'3307',
+    port:'3306',
     user: "root",
     password: "#password1196",
     database:"db_infographics"
